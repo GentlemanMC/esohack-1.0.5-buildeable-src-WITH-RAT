6fec36ac63e938794445cb2e4763550e8f257f24
Gentleman <76640636+GentlemanMC@users.noreply.github.com>
2021-09-24 14:45:30 +0300

    Delete src/main/java/com/squareup/okhttp directory

--- src/main/java/com/squareup/okhttp/Callback.java
+++
@@ -1,9 +0,0 @@
-package com.squareup.okhttp;
-
-import java.io.IOException;
-
-public interface Callback {
-   void onFailure(Request var1, IOException var2);
-
-   void onResponse(Response var1) throws IOException;
-}
